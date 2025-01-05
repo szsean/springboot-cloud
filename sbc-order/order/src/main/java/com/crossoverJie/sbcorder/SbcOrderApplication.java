9f2e90ec273d2cce241982e236511caa616a28d7
zsq <bw_zhangshengqiang@whty.com.cn>
2025-01-05 21:06:36 +0800

    optimize

--- sbc-order/order/src/main/java/com/crossoverJie/sbcorder/SbcOrderApplication.java
+++ sbc-order/order/src/main/java/com/crossoverJie/sbcorder/SbcOrderApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
